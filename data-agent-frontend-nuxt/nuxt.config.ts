d48967bbc24a6ded48d53338643b43eeb942328f
张华 <leftright@live.com>
2026-08-27 14:13:57 +0800

    静态打包配置

--- data-agent-frontend-nuxt/nuxt.config.ts
+++ data-agent-frontend-nuxt/nuxt.config.ts
@@ -16,6 +16,9 @@
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    nitro: {
+		static: true,
+	},
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
 	modules: ['vuetify-nuxt-module', '@pinia/nuxt', '@nuxt/eslint'],
